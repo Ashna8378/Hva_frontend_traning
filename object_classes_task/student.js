0af4ff2f557370167a010f1f5cdbee14f053f649
Ashna123 <ashna22@navgurukul.org>
2024-07-31 11:24:26 +0530

    initial commit

--- object_classes_task/student.js
+++ object_classes_task/student.js
@@ -1,84 +1,84 @@
-// a. Define and Populate the student Object:
+a. Define and Populate the student Object:
 
-// const student = {}
-// student.name = "Ashna"
-// student.email = "dubeyashna123@gmail.com"
-// student.age = 21
+const student = {}
+student.name = "Ashna"
+student.email = "dubeyashna123@gmail.com"
+student.age = 21
 
-// console.log(student)
-
-
-// b. Update the student Object:
-
-// student.age = 10
-
-// console.log(student.age)
+console.log(student)
 
 
-// c. Add Method and Nested Object to student:
+b. Update the student Object:
 
-// student.greet = function(){
-//     console.log(`Hello, ${this.name}!`)
-// }
+student.age = 10
 
-// student.greet()
-
-// student.address = {
-//     country :"India",
-//     city: "New Delhi",
-//     pin_code: 110025
-// }
-
-// console.log(student.address.country)
-
-// student.address.pin_code = 1228833
-
-// console.log(student.address)
+console.log(student.age)
 
 
-// d. Create and Populate the friend Object:
+c. Add Method and Nested Object to student:
+
+student.greet = function(){
+    console.log(`Hello, ${this.name}!`)
+}
+
+student.greet()
+
+student.address = {
+    country :"India",
+    city: "New Delhi",
+    pin_code: 110025
+}
+
+console.log(student.address.country)
+
+student.address.pin_code = 1228833
+
+console.log(student.address)
 
 
-// let friend = {
-//     name: "John Doe",
-//     email: "john.example.com",
-//     age: 21,
-//     address: {
-//         country: "India",
-//         city: "Mumbai",
-//         pin_code: 40001
-
-//     },
-//     greet: function (){
-//         console.log(`Hello ${this.name}!`)
-//     }
-// }
-// friend.greet()
-
-// console.log(friend)
+d. Create and Populate the friend Object:
 
 
-// e. Create and Populate the topper Object:
+let friend = {
+    name: "John Doe",
+    email: "john.example.com",
+    age: 21,
+    address: {
+        country: "India",
+        city: "Mumbai",
+        pin_code: 40001
+
+    },
+    greet: function (){
+        console.log(`Hello ${this.name}!`)
+    }
+}
+friend.greet()
+
+console.log(friend)
 
 
-// let topper = {
-//     name: "khushi",
-//     email: "khushi@gmail.com",
-//     age: 12,
-//     address: {
-//         country: "India",
-//         city: "Mumbai",
-//         pin_code: 40001
-//     },
-//     greet: function(){
-//         console.log(`Hello ${this.name}!`)
-//     }
-
-// }
-
-// topper.greet()
-// console.log(topper)
+e. Create and Populate the topper Object:
 
 
-// f. 
+let topper = {
+    name: "khushi",
+    email: "khushi@gmail.com",
+    age: 12,
+    address: {
+        country: "India",
+        city: "Mumbai",
+        pin_code: 40001
+    },
+    greet: function(){
+        console.log(`Hello ${this.name}!`)
+    }
+
+}
+
+topper.greet()
+console.log(topper)
+
+
+
 
