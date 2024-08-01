72897acacb5c223d8000692e07247516e23d6dbd
Ashna123 <ashna22@navgurukul.org>
2024-08-01 19:25:55 +0530

    initial commit

---
+++ arrays_task/array_methods.js
@@ -0,0 +1,22 @@
+
+// a. Define and Initialize the Array:
+
+numbers = [1,2,3,4,5]
+
+// Call the forEach method to iterate over the numbers array and log each number to the console. Write the callback function as a function expression.
+
+// numbers.forEach(function(number){
+//       console.log(number);
+// })
+
+
+// Call the forEach method to iterate over the numbers array and log each number multiplied by 2 to the console. Write the callback function as a function expression.
+
+
+// numbers.forEach(function(number){
+//     console.log(number*2)
+// })
+
+
+//c. Iterate and Create a New Modified Array Using map():
+
---
+++ arrays_task/colors.js
@@ -0,0 +1,80 @@
+// a Define and Log the colors Array---------------------------------------------------------------------
+
+
+let colors = ["red", "green", "blue"]
+// console.log(colors[0])
+
+
+// b. Modify and Add color elements:
+
+
+// colors[1] = "yellow"
+// console.log(colors[1]) 
+
+
+// colors.push("pink")
+// console.log(colors[colors.length - 1])
+
+
+// c Iterate using Loops over the colors Array-----------------------------------------------------------------------------------------------------
+
+//  for loop---------------------
+
+
+// for(let i = 0; i < colors.length;i=i+1){
+//     console.log(colors[i])
+
+// while loop---------------------------
+
+// let i = 0
+// while (i < colors.length){
+//     console.log(colors[i])
+//     i = i + 1 
+// }
+
+
+// for of loop-------------------------------
+
+// for (let color of colors){
+//     console.log(color)
+// }
+
+// d. Check Array Properties---------------------------------------------------------------------------------------------------------------------
+
+// console.log(typeof(colors))
+
+// console.log(colors.length)
+
+//e. Array Methods------------------------------------------------------------------------------------------------------------------------------------------
+
+// push method------------
+
+// colors.push("black")
+// console.log(colors)
+
+// pop method---------
+
+
+// colors.pop()      // if we store it in a variable thne it will give us deleted value
+// console.log(pop)
+// console.log(colors)
+
+
+// indexOf method--------
+
+// let index = colors.indexOf("blue")
+// console.log(index)
+
+
+// f. Add and Iterate Over Properties:
+
+// colors.owner = "Ashna Dubey"
+// console.log(colors)
+
+
+
+// for(const property in colors){
+//     console.log(`${property} ${colors[property]}`)
+// }
+
+
